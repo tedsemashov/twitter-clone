a05775e9d222597f2ab15c147275b209f36542a0
Ted Semashov <ted.semashov@gmail.com>
2023-05-13 13:29:59 +0200

    added new components

---
+++ src/components/NewTweetForm.tsx
@@ -0,0 +1,54 @@
+import Button from "~/components/Button";
+import ProfileImage from "~/components/ProfileImage";
+import { useSession } from "next-auth/react";
+import { useCallback, useLayoutEffect, useRef, useState } from "react";
+
+const updateTextAreaSize = (textArea?: HTMLTextAreaElement) => {
+  if (textArea == null) return;
+
+  textArea.style.height = "0";
+  textArea.style.height = `${textArea.scrollHeight}px`;
+};
+
+const Form = () => {
+  const textAreaRef = useRef<HTMLTextAreaElement>();
+  const session = useSession();
+  const [inputValue, setInputValue] = useState("");
+
+  const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
+    updateTextAreaSize(textArea);
+    textAreaRef.current = textArea;
+  }, []);
+
+  useLayoutEffect(() => {
+    updateTextAreaSize(textAreaRef.current);
+  }, [inputValue]);
+
+  if (session.status !== "authenticated") return null;
+
+  return (
+    <form className="flex flex-col gap-2 border-b px-4 py-2">
+      <div className="flex gap-4">
+        <ProfileImage src={session.data.user.image} />
+        <textarea
+          ref={inputRef}
+          style={{ height: 0 }}
+          value={inputValue}
+          onChange={({ target }) => setInputValue(target.value)}
+          className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
+          placeholder="What's happening?"
+        />
+      </div>
+      <Button className="self-end">Tweet</Button>
+    </form>
+  );
+};
+
+const NewTweetForm = () => {
+  const session = useSession();
+  if (session.status !== "authenticated") return;
+
+  return <Form />;
+};
+
+export default NewTweetForm;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
+import SideNav from "~/components/SideNav";
 
 import { api } from "~/utils/api";
 
@@ -12,7 +14,20 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Head>
+        <title>Twitter Clone</title>
+        <meta
+          name="description"
+          content="This is a Twitter Clone by Ted Semashov"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container mx-auto flex items-start sm:pr-4">
+        <SideNav />
+        <div className="min-h-screen flex-grow border-x">
+          <Component {...pageProps} />
+        </div>
+      </div>
     </SessionProvider>
   );
 };
